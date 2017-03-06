447de163af4c47fad305868b109052251e39889f
Umair Butt <umair.butt@hotmail.co.uk>
2017-03-06 14:40:49 +0000

    Added comments

--- src/CodingQuestions.CSharp/LowestCommonAncestor.cs
+++ src/CodingQuestions.CSharp/LowestCommonAncestor.cs
@@ -35,18 +35,27 @@ namespace CodingQuestions.CSharp
 
         public static Tree FindLowestCommonAncestor(Tree root, Tree p, Tree q)
         {
+            // Base case
             if (root == null)
                 return null;
 
+            // If either p or q matches with root's key, report
+            // the presence by returning root (Note that if a key is
+            // ancestor of other, then the ancestor key becomes LCA
             if (root == p || root == q)
                 return root;
 
+            // Look for keys in left and right subtrees
             Tree left = FindLowestCommonAncestor(root.Left, p, q);
             Tree right = FindLowestCommonAncestor(root.Right, p, q);
 
+            // If both of the above calls return Non-NULL, then one key
+            // is present in once subtree and other is present in other,
+            // So this node is the LCA
             if (left != null && right != null)
                 return root;
 
+            // Otherwise check if left subtree or right subtree is LCA
             return left != null ? left : right;
         }
     }
